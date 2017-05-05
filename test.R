44ecf6d2a3d472c2a9c3ae797ddc6ad0024dd2b2
keithhurley <keith.l.hurley@gmail.com>
2017-05-05 09:20:12 -0500

    likert playing

--- test.R
+++ test.R
@@ -1,1 +1,26 @@
 #setwd("H:/r/Projects/GitHubTest/")
+
+install.packages("likert")
+library("likert")
+demo('likert', package='likert')
+
+devtools::install_github('jbryer/likert')
+library("likert")
+likert::likert.bar.plot
+
+
+p<-plot(l24)
+p +
+  scale_fill_manual(values=c("blue", "red", "yellow", "green")) + 
+  labs(fill="", title="This is a test.") +
+  theme(plot.background = element_rect(fill="gray80"),
+        panel.grid=element_blank(), 
+        panel.background = element_rect(fill="white"),
+        axis.text=element_text(face="italic"),
+        legend.background = element_rect(fill=NA),
+        legend.key=element_rect(fill=NA, color=NA)
+        ) 
+    
+xtable(l24)
+summary(l24)
+l29
